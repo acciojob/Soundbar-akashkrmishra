744eaf733248a7a78af5373e1b37510a7c14076d
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-19 15:51:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 //your JS code here. If required.
-const applause = document.querySelector("btn");
+const applause = document.querySelector(".btn");
 applause.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const boo = document.querySelector("btn");
+const boo = document.querySelector(".btn");
 boo.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const gasp = document.querySelector("btn");
+const gasp = document.querySelector(".btn");
 gasp.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const tada = document.querySelector("btn");
+const tada = document.querySelector(".btn");
 tada.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const victory = document.querySelector("btn");
+const victory = document.querySelector(".btn");
 victory.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const wrong = document.querySelector("btn");
+const wrong = document.querySelector(".btn");
 wrong.addEventListener("click",() => {
     let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
     mysound.play();
 });
 
-const stop = document.querySelector("btn");
+const stop = document.querySelector(".stop");
 stop.addEventListener("click",() => {
 	mysound.pause();
     mysound.currentTime = 0;
