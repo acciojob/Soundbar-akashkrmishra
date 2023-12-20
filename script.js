0da4179ebee2422078c2b8848754b5aaf9920b83
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-20 10:53:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 const applause = document.querySelector(".applause");
 applause.addEventListener("click",() => {
+	console.log("applause button clicked");
 let mysound = new Audio('http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3');
 mysound.play();
 });
